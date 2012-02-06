40f831b11ba508d97ebf2bc6503ecfe478fbcb83
hrs <hrs113355@gmail.com>
2012-02-06 19:31:49 +0800

    implement json decode

--- index.php
+++ index.php
@@ -3,7 +3,22 @@
     require('php-plurk-api/plurk_api.php');
 
     $payload = json_decode($_POST['payload']);
-    error_log(print_r($payload, true));
+    $repo_name = $payload->repository->name;
+    $commits = $payload->commits;
+    foreach ($commits as $c)
+    {
+	$author = $c->author->name;
+	$url = get_tinyurl($c->url);
+	$msg = $c->message;
+
+	$post_msg = "[$repo_name] $url $author - $msg";
+	doPlurk($post_msg);
+    }
+
+    function get_tinyurl($url)
+    {
+	return file_get_contents('http://tinyurl.com/api-create.php?url='.$url);
+    }
 
     function doPlurk($msg)
     {
