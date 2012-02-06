c6abcaff5102d6b47dfbada59ccb56377f85128d
hrs <hrs113355@gmail.com>
2012-02-06 19:33:13 +0800

    add missing config.php

---
+++ php-plurk-api/config.php
@@ -0,0 +1,15 @@
+<?php
+
+	/**
+	 * for php-plurk-api config
+	 * @package php-plurk-api
+	 *
+	 */
+
+	define('BASE_PATH', dirname(__FILE__) . DIRECTORY_SEPARATOR );
+	define('PLURK_COOKIE_PATH', BASE_PATH . 'cookie');
+	define('PLURK_LOG_PATH', BASE_PATH . 'log');
+
+	define('PLURK_NOT_LOGIN', 'You are not login.');
+	define('PLURK_AGENT', 'php-plurk-api agent');
+?>
